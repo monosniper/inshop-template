10ebd75f37bcd04a91208fd44347a15616b19432
monosniper <vanteateam@gmail.com>
2022-09-11 00:56:07 +0500

    dasd

--- components/SocialNetworks.js
+++ components/SocialNetworks.js
@@ -4,11 +4,11 @@ import shop from "../store/shop";
 const SocialNetworks = () => {
     return (
         <div className="socials">
-            {shop.social_networks.map((social_network, i) => (
+            {shop.social_networks.map((social_network, i) => social_network.value ? (
                 <a href={social_network.value} target={'_blank'} rel={'noreferrer'} key={'social-header-'+i} className="social">
                     <img src={social_network.icon_url} alt={social_network.slug}/>
                 </a>
-            ))}
+            ) : null)}
         </div>
     );
 };
