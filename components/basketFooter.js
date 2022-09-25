48ce54bb76be80db352ff7dc4d1cc79d67770fa6
monosniper <vanteateam@gmail.com>
2022-09-25 20:23:03 +0500

    promocodes

--- components/basketFooter.js
+++ components/basketFooter.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import styles from "../styles/components/BasketFooter.module.scss";
 import basket from "../store/basket";
-import Link from "next/link";
 import {observer} from "mobx-react-lite";
-import {$routes} from "../http/routes";
 import {useTranslation} from "react-i18next";
 import CheckoutBtn from "./CheckoutBtn";
 import {useShop} from "../hooks/useShop";
@@ -18,7 +16,7 @@ const BasketFooter = () => {
                 <span className={styles['basket-footer__text']}>{t('all total')}:</span>
             </div>
             <div className={styles['basket-footer__right']}>
-                <span className={styles['basket-footer__price'] + ' contrast'}>{basket.getSum()+(shop.delivery || 0)}{shop.currency}</span>
+                <span className={styles['basket-footer__price'] + ' contrast'}>{parseInt(basket.getSum())+parseInt((shop.delivery || 0))}{shop.currency}</span>
                 <CheckoutBtn>{t('pay')}</CheckoutBtn>
             </div>
         </div>
